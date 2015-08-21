e884e8efc6f2a56cb654612b25ef619d43dc39b7
Ray Nicholus <rnicholus@users.noreply.github.com>
2015-08-21 12:47:19 -0500

    fix delete call

--- endpoint-cors.php
+++ endpoint-cors.php
@@ -122,8 +122,8 @@ function getS3Client() {
 // Only needed if the delete file feature is enabled
 function deleteObject() {
     getS3Client()->deleteObject(array(
-        'Bucket' => $_POST['bucket'],
-        'Key' => $_POST['key']
+        'Bucket' => $_REQUEST['bucket'],
+        'Key' => $_REQUEST['key']
     ));
 }
 
